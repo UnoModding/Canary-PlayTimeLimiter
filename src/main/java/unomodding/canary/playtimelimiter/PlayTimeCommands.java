1fc7c9d2be1a2c0baf4218d92954b61887d07105
Jamie (Lexteam) <lexwaresupport@outlook.com>
2014-10-11 18:27:27 +0100

    Now using sub-commands!

--- src/main/java/unomodding/canary/playtimelimiter/PlayTimeCommands.java
+++ src/main/java/unomodding/canary/playtimelimiter/PlayTimeCommands.java
@@ -9,6 +9,7 @@ package unomodding.canary.playtimelimiter;
 import java.util.List;
 
 import net.canarymod.Canary;
+import net.canarymod.api.entity.living.humanoid.Player;
 import net.canarymod.chat.MessageReceiver;
 import net.canarymod.chat.TextFormat;
 import net.canarymod.commandsys.Command;
@@ -24,140 +25,132 @@ public class PlayTimeCommands implements CommandListener {
         this.plugin = plugin;
     }
 
-    @Command(aliases = { "playtime", "pt" }, description = "playtime command", permissions = {}, toolTip = "/playtime <start|stop|add|remove|check> [parameters...]", version = 2)
-    public void playtimeCommand(MessageReceiver caller, String[] args) {
-        if (args.length != 0) {
-            if (args[0].equals("start") && args.length == 1) {
-                if (!caller.hasPermission("playtimelimiter.playtime.start")) {
-                    caller.message(TextFormat.RED
-                            + "You don't have permission to start the playtime counter!");
-                } else {
-                    if (!plugin.start()) {
-                        caller.message(TextFormat.RED + "Playtime already started!");
-                    } else {
-                        caller.message(TextFormat.RED + "Playtime started!");
-                    }
-                }
-                return;
-            } else if (args[0].equals("stop")) {
-                if (!caller.hasPermission("playtimelimiter.playtime.stop")) {
-                    caller.message(TextFormat.RED + "You don't have permission to stop the playtime counter!");
-                } else {
-                    if (!plugin.stop()) {
-                        caller.message(TextFormat.RED + "Playtime already stopped!");
-                    } else {
-                        caller.message(TextFormat.RED + "Playtime stopped!");
-                    }
-                }
-                return;
-            } else if (args[0].equals("remove") && args.length == 3) {
-                if (!plugin.hasStarted()) {
-                    caller.message(TextFormat.RED + "Playtime hasn't started yet!");
-                    return;
-                }
-                if (!caller.hasPermission("playtimelimiter.playtime.remove")) {
-                    caller.message(TextFormat.RED
-                            + "You don't have permission to add remove from a players playtime!");
-                } else {
-                    try {
-                        plugin.addPlayTime(Canary.getServer().getPlayer(args[1]).getUUIDString(),
-                                Integer.parseInt(args[2]));
-                        caller.message(TextFormat.GREEN + "Removed " + Integer.parseInt(args[2])
-                                + " seconds of playtime to " + args[1]);
-                    } catch (NumberFormatException e) {
-                        e.printStackTrace();
-                        caller.message(TextFormat.RED + "Invalid number of seconds given!");
-                    }
-                }
-                return;
-            } else if (args[0].equals("add") && args.length == 3) {
-                if (!plugin.hasStarted()) {
-                    caller.message(TextFormat.RED + "Playtime hasn't started yet!");
-                    return;
-                }
-                if (!caller.hasPermission("playtimelimiter.playtime.add")) {
-                    caller.message(TextFormat.RED
-                            + "You don't have permission to add time to a players playtime!");
-                } else {
-                    try {
-                        plugin.removePlayTime(Canary.getServer().getPlayer(args[1]).getUUIDString(),
-                                Integer.parseInt(args[2]));
-                        caller.message(TextFormat.GREEN + "Added " + Integer.parseInt(args[2])
-                                + " seconds of playtime from " + args[1]);
-                    } catch (NumberFormatException e) {
-                        e.printStackTrace();
-                        caller.message(TextFormat.RED + "Invalid number of seconds given!");
-                    } catch (UnknownPlayerException e) {
-                        e.printStackTrace();
-                        caller.message(TextFormat.RED + e.getMessage());
-                    }
-                }
-                return;
-            } else if (args[0].equals("check")) {
-                if (!plugin.hasStarted()) {
-                    caller.message(TextFormat.RED + "Playtime hasn't started yet!");
-                }
-                if (args.length == 1) {
-                    if (!caller.hasPermission("playtimelimiter.playtime.check.self")) {
-                        caller.message(TextFormat.RED + "You don't have permission to check your playtime!");
-                    } else {
-                        caller.message(TextFormat.GREEN
-                                + "You have played for "
-                                + plugin.secondsToDaysHoursSecondsString(plugin.getPlayerPlayTime(Canary
-                                        .getServer().getPlayer(caller.getName()).getUUIDString()))
-                                + " and have "
-                                + plugin.secondsToDaysHoursSecondsString(plugin
-                                        .getTimeAllowedInSeconds(Canary.getServer()
-                                                .getPlayer(caller.getName()).getUUIDString()
-                                                + " remaining!")));
-                    }
-                    return;
-                } else if (args.length == 2) {
-                    if (!caller.hasPermission("playtimelimiter.playtime.check.others")) {
-                        caller.message(TextFormat.RED
-                                + "You don't have permission to check other players playtime!");
-                    } else {
-                        caller.message(TextFormat.GREEN
-                                + Canary.getServer().getPlayer(args[1]).getName()
-                                + " has played for "
-                                + plugin.secondsToDaysHoursSecondsString(plugin.getPlayerPlayTime(Canary
-                                        .getServer().getPlayer(args[1]).getUUIDString()))
-                                + " and has "
-                                + plugin.secondsToDaysHoursSecondsString(plugin
-                                        .getTimeAllowedInSeconds(Canary.getServer().getPlayer(args[1])
-                                                .getUUIDString())) + " remaining!");
-                    }
-                    return;
-                }
-            }
-        }
+    @Command(aliases = { "playtime", "pt" },
+             description = "playtime command",
+             permissions = {},
+             toolTip = "/playtime <start|stop|add|remove|check> [parameters...]",
+             version = 2)
+    public void baseCommand(MessageReceiver caller, String[] args) {
         printUsage(caller);
     }
 
-    public void printUsage(MessageReceiver sender) {
-        sender.message(TextFormat.YELLOW + "/playtime usage:");
-        if (sender.hasPermission("playtimelimiter.playtime.start")) {
-            sender.message(TextFormat.CYAN + "/playtime start" + TextFormat.RESET
-                    + " - Start the playtime counter.");
+    @Command(aliases = { "start" },
+             parent = "playtime",
+             description = "start subcommand",
+             permissions = { "playtimelimiter.playtime.start" },
+             toolTip = "/playtime start",
+             version = 2)
+    public void startCommand(MessageReceiver caller, String[] args) {
+        if (!plugin.start()) {
+            caller.message(TextFormat.RED + "Playtime already started!");
+        } else {
+            caller.message(TextFormat.RED + "Playtime started!");
         }
-        if (sender.hasPermission("playtimelimiter.playtime.stop")) {
-            sender.message(TextFormat.CYAN + "/playtime stop" + TextFormat.RESET
-                    + " - Stop the playtime counter.");
+    }
+
+    @Command(aliases = { "stop" },
+             parent = "playtime",
+             description = "stop subcommand",
+             permissions = { "playtimelimiter.playtime.stop" },
+             toolTip = "/playtime stop",
+             version = 2)
+    public void stopCommand(MessageReceiver caller, String[] args) {
+        if (!plugin.stop()) {
+            caller.message(TextFormat.RED + "Playtime already stopped!");
+        } else {
+            caller.message(TextFormat.RED + "Playtime stopped!");
         }
-        if (sender.hasPermission("playtimelimiter.playtime.add")) {
-            sender.message(TextFormat.CYAN + "/playtime add <user> <time>" + TextFormat.RESET
-                    + " - Add time in seconds to the user's playtime.");
+    }
+
+    @Command(aliases = { "check" },
+             parent = "playtime",
+             description = "stop subcommand",
+             permissions = {},
+             toolTip = "/playtime check [player]",
+             version = 2)
+    public void checkCommand(MessageReceiver caller, String[] args) {
+        if (!plugin.hasStarted()) {
+            caller.message(TextFormat.RED + "Playtime hasn't started yet!");
+        } else {
+            if (args.length == 0) {
+                if (!caller.hasPermission("playtimelimiter.playtime.check.self")) {
+                    caller.message(TextFormat.RED + "You don't have permission to check your playtime!");
+                } else {
+                    Player player = Canary.getServer().getPlayer(caller.getName());
+                    caller.message(TextFormat.GREEN
+                            + "You have played for "
+                            + plugin.secondsToDaysHoursSecondsString(plugin.getPlayerPlayTime(player
+                                    .getUUIDString()))
+                            + " and have "
+                            + plugin.secondsToDaysHoursSecondsString(plugin.getTimeAllowedInSeconds(player
+                                    .getUUIDString())) + " remaining!");
+                }
+            } else if (args.length == 1) {
+                if (!caller.hasPermission("playtimelimiter.playtime.check.others")) {
+                    caller.message(TextFormat.RED
+                            + "You don't have permission to check other players playtime!");
+                } else {
+                    Player player = Canary.getServer().getPlayer(args[0]);
+                    caller.message(TextFormat.GREEN
+                            + player.getName()
+                            + " has played for "
+                            + plugin.secondsToDaysHoursSecondsString(plugin.getPlayerPlayTime(player
+                                    .getUUIDString()))
+                            + " and has "
+                            + plugin.secondsToDaysHoursSecondsString(plugin.getTimeAllowedInSeconds(player
+                                    .getUUIDString())) + " remaining!");
+                }
+            }
         }
-        if (sender.hasPermission("playtimelimiter.playtime.check.others")) {
-            sender.message(TextFormat.CYAN + "/playtime check [user]" + TextFormat.RESET
-                    + " - Check the time played and time left for a given user, or if blank, for yourself.");
-        } else if (sender.hasPermission("playtimelimiter.playtime.check.self")) {
-            sender.message(TextFormat.CYAN + "/playtime check" + TextFormat.RESET
-                    + " - Check the time played and time left for yourself.");
+    }
+
+    @Command(aliases = { "add" },
+             parent = "playtime",
+             description = "add subcommand",
+             permissions = { "playtimelimiter.playtime.add" },
+             toolTip = "/playtime add [seconds]",
+             version = 2)
+    public void addCommand(MessageReceiver caller, String[] args) {
+        if (!plugin.hasStarted()) {
+            caller.message(TextFormat.RED + "Playtime hasn't started yet!");
+        } else {
+            try {
+                plugin.removePlayTime(Canary.getServer().getPlayer(args[0]).getUUIDString(),
+                        Integer.parseInt(args[0]));
+                caller.message(TextFormat.GREEN + "Added " + Integer.parseInt(args[1])
+                        + " seconds of playtime from " + args[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                caller.message(TextFormat.RED + "Invalid number of seconds given!");
+            } catch (UnknownPlayerException e) {
+                e.printStackTrace();
+                caller.message(TextFormat.RED + e.getMessage());
+            }
         }
-        if (sender.hasPermission("playtimelimiter.playtime.remove")) {
-            sender.message(TextFormat.CYAN + "/playtime remove <user> <time>" + TextFormat.RESET
-                    + " - Remove time in seconds from the user's playtime.");
+    }
+
+    @Command(aliases = { "remove" },
+             parent = "playtime",
+             description = "remove subcommand",
+             permissions = { "playtimelimiter.playtime.remove" },
+             toolTip = "/playtime remove [seconds]",
+             version = 2)
+    public void removeCommand(MessageReceiver caller, String[] args) {
+        if (!plugin.hasStarted()) {
+            caller.message(TextFormat.RED + "Playtime hasn't started yet!");
+        } else {
+            try {
+                plugin.removePlayTime(Canary.getServer().getPlayer(args[0]).getUUIDString(),
+                        Integer.parseInt(args[1]));
+                caller.message(TextFormat.GREEN + "Added " + Integer.parseInt(args[1])
+                        + " seconds of playtime from " + args[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                caller.message(TextFormat.RED + "Invalid number of seconds given!");
+            } catch (UnknownPlayerException e) {
+                e.printStackTrace();
+                caller.message(TextFormat.RED + e.getMessage());
+            }
         }
     }
 
@@ -172,6 +165,33 @@ public class PlayTimeCommands implements CommandListener {
             return TabCompleteHelper.matchTo(parameters, Canary.getServer().getKnownPlayerNames());
         } else {
             return null;
+        }
+    }
+
+    public void printUsage(MessageReceiver caller) {
+        caller.message(TextFormat.YELLOW + "/playtime usage:");
+        if (caller.hasPermission("playtimelimiter.playtime.start")) {
+            caller.message(TextFormat.CYAN + "/playtime start" + TextFormat.RESET
+                    + " - Start the playtime counter.");
+        }
+        if (caller.hasPermission("playtimelimiter.playtime.stop")) {
+            caller.message(TextFormat.CYAN + "/playtime stop" + TextFormat.RESET
+                    + " - Stop the playtime counter.");
+        }
+        if (caller.hasPermission("playtimelimiter.playtime.add")) {
+            caller.message(TextFormat.CYAN + "/playtime add <user> <time>" + TextFormat.RESET
+                    + " - Add time in seconds to the user's playtime.");
+        }
+        if (caller.hasPermission("playtimelimiter.playtime.check.others")) {
+            caller.message(TextFormat.CYAN + "/playtime check [user]" + TextFormat.RESET
+                    + " - Check the time played and time left for a given user, or if blank, for yourself.");
+        } else if (caller.hasPermission("playtimelimiter.playtime.check.self")) {
+            caller.message(TextFormat.CYAN + "/playtime check" + TextFormat.RESET
+                    + " - Check the time played and time left for yourself.");
+        }
+        if (caller.hasPermission("playtimelimiter.playtime.remove")) {
+            caller.message(TextFormat.CYAN + "/playtime remove <user> <time>" + TextFormat.RESET
+                    + " - Remove time in seconds from the user's playtime.");
         }
     }
 }
